4c46d7bee35bcade91050260fe617c042357a8cb
Quinten Ceuppens <quintenceuppens@Quintens-MacBook-Pro.local>
2024-08-22 16:13:25 +0200

    next commit

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def main():
-    st.title("Deployed with Appliku")
-    st.write("This app is deployed with Appliku, hey!!!")
+    st.title("Deployed with Render")
+    st.write("This app is deployed with Render")
 
 if __name__ == "__main__":
     main()
